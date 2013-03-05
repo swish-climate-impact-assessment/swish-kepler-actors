f6e0b3a9d7ea2f24f458d986e017a94663d00a25
ian <ootff@yahoo.com.au>
2013-03-05 17:01:11 +1100

    Swish Added a bunch of logic actors, less/greater, and/or etc

---
+++ Swish/StataOperations/LessThanOrEqualValueOperation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System;
+using Swish.Adapters;
+
+namespace Swish.StataOperations
+{
+	public class LessThanOrEqualValueOperation: IStataBasedOperation
+	{
+		public string Name { get { return "LessThanOrEqualValue"; } }
+
+		public const string InputName = "input";
+		public List<string> InputNames
+		{
+			get
+			{
+				List<string> inputs = new List<string>();
+				inputs.Add(InputName);
+				return inputs;
+			}
+		}
+
+		public bool ProducesOutputFile { get { return true; } }
+
+		public List<string> GenerateScript(AdapterArguments adapterArguments, SortedList<string, string> inputFileNames, string outputFileName)
+		{
+			List<string> lines = new List<string>();
+
+			string inputFileName = inputFileNames[InputName];
+			StataScriptFunctions.LoadFileCommand(lines, inputFileName);
+
+			double value = adapterArguments.Double("value", true);
+			string variableName = adapterArguments.String("variable", true);
+
+			string resultVariableName = adapterArguments.String("resultVariable", false);
+			if (string.IsNullOrWhiteSpace(resultVariableName))
+			{
+				resultVariableName = AdapterFunctions.WorkingVariableName;
+			}
+
+			string expression = variableName + " <= " + value;
+			StataScriptFunctions.Generate(lines, StataDataType.Byte, resultVariableName, expression);
+
+			StataScriptFunctions.SaveFileCommand(lines, outputFileName);
+
+			return lines;
+		}
+
+	}
+}
---
+++ Swish/StataOperations/LessThanVariableOperation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System;
+using Swish.Adapters;
+
+namespace Swish.StataOperations
+{
+	public class LessThanVariableOperation: IStataBasedOperation
+	{
+		public string Name { get { return "LessThanVariable"; } }
+
+		public const string InputName = "input";
+		public List<string> InputNames
+		{
+			get
+			{
+				List<string> inputs = new List<string>();
+				inputs.Add(InputName);
+				return inputs;
+			}
+		}
+
+		public bool ProducesOutputFile { get { return true; } }
+
+		public List<string> GenerateScript(AdapterArguments adapterArguments, SortedList<string, string> inputFileNames, string outputFileName)
+		{
+			List<string> lines = new List<string>();
+
+			string inputFileName = inputFileNames[InputName];
+			StataScriptFunctions.LoadFileCommand(lines, inputFileName);
+
+			string leftVariableName = adapterArguments.String("leftVariable", true);
+			string rightVariableName = adapterArguments.String("rightVariable", true);
+
+			string resultVariableName = adapterArguments.String("resultVariable", false);
+			if (string.IsNullOrWhiteSpace(resultVariableName))
+			{
+				resultVariableName = AdapterFunctions.WorkingVariableName;
+			}
+
+			string expression = leftVariableName + " < " + rightVariableName;
+			StataScriptFunctions.Generate(lines, StataDataType.Double, resultVariableName, expression);
+
+			StataScriptFunctions.SaveFileCommand(lines, outputFileName);
+
+			return lines;
+		}
+
+	}
+}
