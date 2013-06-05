2c182e20805e559aa2ff65c30fb8651f6fedc50c
ivanhanigan <ivan.hanigan@gmail.com>
2013-06-05 13:31:45 +1000

    added R download actor

--- R_simple_installer.r
+++ R_simple_installer.r
@@ -1,7 +1,20 @@
 # copy actors to kepler
+destination <- "~/KeplerData/workflows/MyWorkflows/"
+filelist <- dir(destination)
+for(fname in filelist)  
+{
+  file.remove(file.path(destination, fname))
+}
 basedir <- "SimpleInstaller/Actors"
 filelist <- dir(basedir)
-for(fname in filelist)
+for(fname in filelist)  
 {
-  file.copy(file.path(basedir,fname), file.path("~/KeplerData/workflows/MyWorkflows/", fname))
+  file.copy(file.path(basedir,fname), file.path(destination, fname))
+}
+basedir <- "SimpleInstaller/SampleData"
+filelist <- dir(basedir)
+destination <- "~/KeplerData/MyData"
+for(fname in filelist)  
+{
+  file.copy(file.path(basedir,fname), file.path(destination, fname))
 }
