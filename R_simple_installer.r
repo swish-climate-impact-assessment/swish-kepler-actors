8c24dc17c51b84a3eea9cd51432c32325a90b19e
ivanhanigan <ivan.hanigan@gmail.com>
2013-06-17 11:56:31 +1000

    add notes re simple installer

--- R_simple_installer.r
+++ R_simple_installer.r
@@ -1,7 +1,27 @@
 # copy actors to kepler
+# NB for Ian's windows installer need to:
+# add new actor to actors dir in /simpleinstaller
+# copy to /SWISHKeplerActorsInstallerDDMMMMYYYY/Bin
+# along with the installer exe stuff
+# make shortcuts
+# zip up and should work
+
+destination <- "~/KeplerData/workflows/MyWorkflows/"
+filelist <- dir(destination)
+for(fname in filelist)  
+{
+  file.remove(file.path(destination, fname))
+}
 basedir <- "SimpleInstaller/Actors"
 filelist <- dir(basedir)
-for(fname in filelist)
+for(fname in filelist)  
 {
-  file.copy(file.path(basedir,fname), file.path("~/KeplerData/workflows/MyWorkflows/", fname))
+  file.copy(file.path(basedir,fname), file.path(destination, fname))
+}
+basedir <- "SimpleInstaller/SampleData"
+filelist <- dir(basedir)
+destination <- "~/KeplerData/MyData"
+for(fname in filelist)  
+{
+  file.copy(file.path(basedir,fname), file.path(destination, fname))
 }
