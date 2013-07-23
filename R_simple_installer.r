1928b40fe18acda46bc5dbc528c5f003ed623c68
ivanhanigan <ivan.hanigan@gmail.com>
2013-07-23 20:30:04 +1000

    add R_sql_subset

--- R_simple_installer.r
+++ R_simple_installer.r
@@ -8,20 +8,21 @@
 
 destination <- "~/KeplerData/workflows/MyWorkflows/"
 filelist <- dir(destination)
-for(fname in filelist)  
+for(fname in filelist)
 {
   file.remove(file.path(destination, fname))
 }
-basedir <- "SimpleInstaller/Actors"
+basedir <- "Swish.SimpleInstaller/Actors"
 filelist <- dir(basedir)
-for(fname in filelist)  
+for(fname in filelist)
 {
+  filelist
   file.copy(file.path(basedir,fname), file.path(destination, fname))
 }
 basedir <- "SimpleInstaller/SampleData"
 filelist <- dir(basedir)
 destination <- "~/KeplerData/MyData"
-for(fname in filelist)  
+for(fname in filelist)
 {
   file.copy(file.path(basedir,fname), file.path(destination, fname))
 }
